b2694b0e65f3a5b0ebaaebf98e3b265f173a8dfe
jaca <jaca@gmail.com>
2020-04-16 21:29:16 +0200

    add TODO, TODOS components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent, useState, Component } from "react";
+import logo from "./logo.svg";
+import { TODOS } from "./components/TODOS";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TODOS />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
